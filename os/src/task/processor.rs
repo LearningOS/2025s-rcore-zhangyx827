fb54fa6f0db7d4706e727cd400ef593cae80380e
zyx <zhangyx@gmail.com>
2025-07-17 20:48:21 +0800

    update

--- os/src/task/processor.rs
+++ os/src/task/processor.rs
@@ -42,7 +42,8 @@ impl Processor {
 
     ///Get current task in cloning semanteme
     pub fn current(&self) -> Option<Arc<TaskControlBlock>> {
-        self.current.as_ref().map(Arc::clone)
+        self.current.as_ref()
+        .map(Arc::clone)
     }
 }
 
